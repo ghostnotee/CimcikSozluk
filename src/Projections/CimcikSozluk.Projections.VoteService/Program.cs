2ccd5f08837b7c9e363977ea11de54836e8c49ab
Selçuk Bilgen <selcukbilgen@hotmail.com>
2022-05-30 15:32:27 +0300

    project structure created

---
+++ src/Projections/CimcikSozluk.Projections.VoteService/Program.cs
@@ -0,0 +1,7 @@
+using CimcikSozluk.Projections.VoteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
